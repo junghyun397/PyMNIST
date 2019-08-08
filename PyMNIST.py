a627514c3fde94f00a27bbb01d931c0975ebd10b
junghyun397 <junghyun397@gmail.com>
2019-08-08 16:26:04 +0900

    Remove bias and setup.py has support Windows system

--- PyMNIST.py
+++ PyMNIST.py
@@ -8,7 +8,6 @@ from random import random
 rand_range = (lambda r: int(random() * r // 1))
 
 weight = [random() * 0.1 * 2 - 0.1 for _ in range(28 * 28 * 10)]
-bias = [random() * 0.01 for _ in range(10)]
 
 
 def forward(x):
@@ -43,7 +42,7 @@ read_file = (lambda fd_name: open("resource/" + fd_name, "rb"))
 
 fd_train_image, fd_train_label = read_file("train-images.idx3-ubyte"), read_file("train-labels.idx1-ubyte")
 fd_test_image, fd_test_label = read_file("t10k-images.idx3-ubyte"), read_file("t10k-labels.idx1-ubyte")
-fd_train_image.read(16); fd_train_label.read(8); fd_test_image.read(16); fd_test_label.read(8)
+fd_train_image.read(16), fd_train_label.read(8), fd_test_image.read(16), fd_test_label.read(8)
 
 train_size, test_size = 60000, 10000
 train_image = [[byte_2_int(fd_train_image.read(1)) / 255 for _ in range(28 * 28)] for _ in range(train_size)]
@@ -60,8 +59,8 @@ _, vis_count = print("===Success loading MNIST data set!===", "\nEnter visualise
 for seq in range(vis_count):
     index = rand_range(train_size)
     print("Label:", train_label[index], "Index:", index, "Sequence:", seq + 1)
-    for j in range(28):
-        print("".join(["#" if train_image[index][j * 28 + f] > 0 else "." for f in range(28)]))
+    for vx in range(28):
+        print("".join(["#" if train_image[index][vx * 28 + vy] > 0 else "." for vy in range(28)]))
 
 _, total_epoch = print("===End visualize data set===========", "\nEnter epoch count:"), int(input())
 print("===Start Training...================")
@@ -75,9 +74,9 @@ for epoch in range(total_epoch):
         y[train_label[index]] = 1.0
         l_cost += backward(train_image[index], x, y, lr=0.002)
         if seq % 500 == 0 and seq != 0:
-            acc, l_cost = check_acc(test_image, test_label, ratio=.05) * 100, l_cost / 500
-            train_history[min(10 * test_size, epoch * train_size + seq)] = acc, l_cost
-            print("accuracy:", str(acc) + "%", "mean-cost:", l_cost)
+            acc, m_cost = check_acc(test_image, test_label, ratio=.05) * 100, l_cost / 500
+            train_history[min(300, len(train_history))] = acc, m_cost
+            print("accuracy:", str(acc) + "%", "mean-cost:", m_cost)
             prc, l_cost = int(seq / train_size * 100 // 2), 0
             print("epoch:", str(epoch+1)+"/"+str(total_epoch), "["+"".join([">"]*prc)+"".join(["."]*(50-prc))+"]")
 
@@ -88,10 +87,10 @@ for seq in range(vis_count):
     index = rand_range(len(valid_label))
     x = forward(valid_image[index])
     print("Prediction:", x.index(max(x)), "Label:", valid_label[index])
-    for j in range(28):
-        print("".join(["#" if valid_image[index][j * 28 + f] > 0 else "." for f in range(28)]))
+    for vx in range(28):
+        print("".join(["#" if valid_image[index][vx * 28 + vy] > 0 else "." for vy in range(28)]))
 
-print("===Print training history....=======", "\n#: cost @: accuracy")
+print("===Print training history....=======\nPress ENTER to continue..."), input(), print("@: accuracy, #: cost")
 acc_history, cost_history = [dt[1][0] for dt in train_history.items()], [dt[1][1] for dt in train_history.items()]
 get_pixel = (lambda va, vc, y: "@" if va/100*40 // 1 == y else ("#" if vc/max(cost_history)*40 // 1 == y else "."))
 for y in reversed(range(1, 41)):
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-import os, shutil, requests, gzip
+import os, shutil, urllib.request, gzip, platform
 
 directory, main_url = "./resource/", "http://yann.lecun.com/exdb/mnist/"
 py_mnist_url = "http://github.com/junghyun397/PyMNIST/blob/master/PyMNIST.py?raw=true"
-p_py_mnist = "PyMNIST.py"
+p_py_mnist = "mnist.py"
 
 p_train_image = "train-images-idx3-ubyte.gz", "train-images.idx3-ubyte"
 p_train_label = "train-labels-idx1-ubyte.gz", "train-labels.idx1-ubyte"
@@ -14,22 +14,23 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 
 print("===Start download PyMNIST script =========================")
-req, compressed = requests.get(url=py_mnist_url), open(p_py_mnist, "wb")
-compressed.write(req.content); compressed.close()
+urllib.request.urlretrieve(py_mnist_url, p_py_mnist)
 print("===Success download PyMNIST script =======================")
 
 for path in path_list:
     print("===Start download file:", path[0], "".join(["=" for _ in range(33 - len(path[0]))]))
-    req, compressed = requests.get(url=main_url + path[0]), open(directory + path[0], "wb")
-    compressed.write(req.content); compressed.close()
+    urllib.request.urlretrieve(main_url + path[0], directory + path[0])
     print("Success download file:", path[0])
 
     print("Start extract gzip file:", path[1])
-    fc = gzip.open(directory + path[0], "rb")
-    nf = open(directory + path[1], "wb")
-    shutil.copyfileobj(fc, nf)
-    nf.close(), fc.close(); os.remove(directory + path[0])
+    fc, nf = gzip.open(directory + path[0], "rb"), open(directory + path[1], "wb")
+    shutil.copyfileobj(fc, nf); nf.close(), fc.close(); os.remove(directory + path[0])
     print("===Success extract gzip file:", path[1], "".join(["=" for _ in range(27 - len(path[1]))]))
 
-print("Press ENTER to automatic run PyMNIST.py or press Ctrl + C to exit"), input()
-os.system("clear"); os.system("python3 " + p_py_mnist)
+print("===Success download all files and script! ================")
+if platform.system() == "Windows":
+    print("Run", p_py_mnist, "to continue....")
+else:
+    _, inp = print("Press ENTER to automatic run ", p_py_mnist, "or type pypy to run with pypy3"), input()
+    os.system("clear"); os.system("python3 " + p_py_mnist) \
+        if inp != "pypy" else os.system("clear"); os.system("pypy3 " + p_py_mnist)
